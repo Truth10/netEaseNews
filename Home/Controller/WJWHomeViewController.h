47a84d05def8e876b76e33b19b4a517f5a7ef91d
Truth10 <mac@WangJianWendeMacBook.local>
2016-06-12 21:46:33 +0800

    1.创建网易彩票项目

---
+++ Home/Controller/WJWHomeViewController.h
@@ -0,0 +1,13 @@
+//
+//  WJWHomeViewController.h
+//  netEaseNews
+//
+//  Created by mac on 16/6/12.
+//  Copyright © 2016年 itheima. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface WJWHomeViewController : UIViewController
+
+@end
---
+++ netEaseNews/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  netEaseNews
+//
+//  Created by mac on 16/6/12.
+//  Copyright © 2016年 itheima. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
